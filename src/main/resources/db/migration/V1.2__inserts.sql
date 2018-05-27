9cf1b2cbb16290dbe93867f2fe87e2b1aa07ef9f
pasha9797 <pasha.97.97@mail.ru>
2018-05-28 00:01:06 +0300

    ?

---
+++ src/main/resources/db/migration/V1.2__inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO user_groups(name) values('Unconfirmed');
+INSERT INTO user_groups(name) values('Default');
+INSERT INTO user_groups(name) values('Admin');
+INSERT INTO user_groups(name) values('Business');
+
+INSERT INTO photo_type(name) values('spot');
+
+INSERT INTO offer_category(name) values('Entertainment');
+INSERT INTO offer_category(name) values('Food');
+INSERT INTO offer_category(name) values('Education');
+INSERT INTO offer_category(name) values('Job');
+INSERT INTO offer_category(name) values('Hobby');
